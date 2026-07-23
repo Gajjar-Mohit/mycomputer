4f66da4c929e89d874ca74ceb5ff9f0e172d44e6
Gajjar-Mohit <gajjarmohit502@gmail.com>
2026-07-23 22:29:22 +0530

    refactor: extract gate function declarations to header and move testing logic to main.c

---
+++ gates.h
@@ -0,0 +1,12 @@
+#ifndef GATES
+#define GATES
+
+int AND(int a, int b);
+int OR(int, int);
+int NOT(int a);
+int NOR(int a, int b);
+int NAND(int a, int b);
+int XOR(int a, int b);
+int XNOR(int a, int b);
+
+#endif
